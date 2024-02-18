0bd6670b278a4370940802d79ab8c541d5fa35e7
Shahriar <shahriar3809@gmail.com>
2024-02-18 20:20:57 +0600

    Added more functionalities

--- script.js
+++ script.js
@@ -21,7 +21,30 @@ for(let seat of allSeat) {
 
                 const increased = document.getElementById('incresed');
                 increase++;
-                increased.innerText = increase;     
+                increased.innerText = increase;  
+                
+
+               const displaySit = document.getElementById('displaySeat');
+               
+               const p = document.createElement('p');
+               const p2 = document.createElement('p');
+               const p3 = document.createElement('p');
+               p.innerText = event.target.innerText;
+               p2.innerText = "Economy";
+               p3.innerText = "550 Tk";
+               const dov = document.createElement('div');
+               dov.classList.add('flex');
+               dov.classList.add('justify-between')
+               dov.appendChild(p)
+               dov.appendChild(p2)
+               dov.appendChild(p3)
+               displaySit.appendChild(dov);
+               
+                
+
+
+
+
                 if(increase === 4) {
                     document.getElementById('applyBtn').removeAttribute('disabled');
                 } else {
@@ -33,6 +56,8 @@ for(let seat of allSeat) {
             total = increase * seatPrice;
             const totalAmount = document.getElementById('total');
             totalAmount.innerText = total;   
+            const grandTotal = document.getElementById('grandTo');
+            grandTotal.innerText = total;
         })
 }
 
@@ -58,8 +83,12 @@ if(inputValue.toUpperCase() === "NEW15") {
         console.log("15")
         document.getElementById('couponArea').classList.add('hidden');
 
+        const grandTotalAmount = document.getElementById('grandTo');
+        const grandTotal =grandTotalAmount.innerText;
+        let grand = parseInt(grandTotal);
+        grand = grand - discount;
+        grandTotalAmount.innerText = grand;
 
-        
 
     
 } else  if(inputValue.toUpperCase() === "COUPLE20"){
@@ -71,7 +100,39 @@ if(inputValue.toUpperCase() === "NEW15") {
         displayDisc.appendChild(p);
         console.log("20")
         document.getElementById('couponArea').classList.add('hidden');
-} else {
 
-}
+        
+        const grandTotalAmount = document.getElementById('grandTo');
+        const grandTotal =grandTotalAmount.innerText;
+        let grand = parseInt(grandTotal);
+        grand = grand - discount;
+        grandTotalAmount.innerText = grand;
+} 
+})
+
+
+
+const phone = document.getElementById('phoneNumber');
+
+phone.addEventListener('keyup', function (event) {
+    const digit = event.target.value;
+    const digitOfInput = digit.length;
+    if(digitOfInput > 0 && digitOfInput <= 11 && increase > 0) {
+        document.getElementById('submitBtn').removeAttribute("disabled", true);
+    } else {
+         document.getElementById('submitBtn').setAttribute("disabled", true);
+    }
+})
+
+
+
+
+
+
+
+document.getElementById('submitBtn').addEventListener ('click', function () {
+    document.getElementById('header').classList.add('hidden');
+    document.getElementById('main').classList.add('hidden');
+    document.getElementById('footer').classList.add('hidden');
+    document.getElementById('modals').classList.remove('hidden');
 })
